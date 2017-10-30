b320fb14de24bb137d90d872a9498af2fc145cfc
alex-stanciu <stanciu.alex.13@gmail.com>
2017-10-30 20:18:04 +0200

    Code style fixes

--- src/Normalizer/EventNormalizer.php
+++ src/Normalizer/EventNormalizer.php
@@ -12,24 +12,24 @@ use Drupal\serialization\Normalizer\NormalizerBase;
  */
 class EventNormalizer extends NormalizerBase {
 
-    /**
-     * {@inheritdoc}
-     */
-    protected $supportedInterfaceOrClass = 'Drupal\node\NodeInterface';
+  /**
+   * {@inheritdoc}
+   */
+  protected $supportedInterfaceOrClass = 'Drupal\node\NodeInterface';
 
-    /**
-     * {@inheritdoc}
-     */
-    public function normalize($object, $format = null, array $context = [])
-    {
-        $attributes = $object->toArray();
-        if ($object instanceof Node && $object->getType() === 'event') {
-            $attributes = [
-                'title' => $object->title->value,
-                'body' => $object->field_body->value,
-                'date' => $object->field_date->value,
-            ];
-        }
-        return $attributes;
+  /**
+   * {@inheritdoc}
+   */
+  public function normalize($object, $format = NULL, array $context = []) {
+    $attributes = $object->toArray();
+    if ($object instanceof Node && $object->getType() === 'event') {
+      $attributes = [
+        'title' => $object->title->value,
+        'body' => $object->field_body->value,
+        'date' => $object->field_date->value,
+      ];
     }
+    return $attributes;
+  }
+
 }
